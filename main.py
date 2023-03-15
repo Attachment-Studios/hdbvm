75d5cf476fc5a1ea68025ee771956243ba6a1308
Attachment Aditya <104914986+AttAditya@users.noreply.github.com>
2023-03-15 21:41:45 +0530

    random changes go

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def response(content: str) -> Response:
 
 image_captcha = ImageCaptcha(400, 150, font_sizes=(105, 125, 140))
 
-deta = Deta("c0JDpq54AxNw_GYLzK9oVP7Mh5ZSNoXzMzdsmUsuLHDp1")
+deta = Deta("")
 data = deta.Base("data")
 drive = deta.Drive("CAPTCHA")
 
